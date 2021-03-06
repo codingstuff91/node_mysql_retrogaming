dcb7a24c295e49918ecf0a5fbc9f9e01d327afa1
Matt MARTIN <codingstuff91@gmail.com>
2021-03-06 20:07:16 +0100

    Add dotenv package and .env.example config file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gamesRouter = require('./routes/games');
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 var app = express();
 
 // view engine setup
