975962c5b344732560379ddbcb5dc0f375de2230
Matt MARTIN <codingstuff91@gmail.com>
2021-03-07 17:07:00 +0100

    delete tools example file and rename users route on user

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
 var gamesRouter = require('./routes/games');
 
 const PORT = process.env.PORT || 3000
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 app.use('/games',gamesRouter);
 
 // catch 404 and forward to error handler
--- config/tools.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    example : "test",
-    connect : ()=>{
-        return "hello"
-    }
-}
