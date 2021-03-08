8c95ef78775aa76918bcd9390f82386ba81725f8
Matthieu MARTIN <codingstuff91@gmail.com>
2021-03-08 19:00:52 +0100

    Add login view and authentication logic

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../config/db');
+var bcrypt = require('bcryptjs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,8 +17,9 @@ router.post('/register', (req,res) => {
   errors = {}
 
   if(req.body.username == ""){
-    errors.username = "Veuillez saisir un nom d'utilisateur"
+    errors.email = "Veuillez saisir un nom d'utilisateur"
   }
+
   if(req.body.email == ""){
     errors.email = "Veuillez saisir une adresse mail"
   }
@@ -26,16 +29,55 @@ router.post('/register', (req,res) => {
   }
   
   if(req.body.username != "" && req.body.email != "" && req.body.password != ""){
-    res.render('register', {success : 'Bravo !! Vous etes maintenant inscrit sur le site !'})
-  }
 
-  console.log(errors);
-  res.render('register', {validation_errors : errors})
+    const username = req.body.username
+    const email  = req.body.email
+    const password = bcrypt.hashSync(req.body.password, 8);
+
+    user_query = `INSERT INTO users (email, username, password) VALUES ("${email}", "${username}", "${password}")`
+
+    db.connect(function(err) {
+
+        db.query(user_query, function (err, result) {
+            console.log('erreurs DB',err);
+            res.render('register', {success : 'Bravo !! Vous etes maintenant inscrit sur le site !'})
+        });
+
+    });
+
+  }else{
+    res.render('register', {validation_errors : errors})
+  }
 
 })
 
 router.get('/login', (req,res) => {
-  res.send('login route')
+  res.render('login')
+});
+
+router.post('/login', (req,res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const hashed_password = bcrypt.hashSync(req.body.password, 8)
+
+  user_query = `SELECT * FROM users WHERE email="${email}"`
+
+  db.connect(function(err) {
+
+    db.query(user_query, function (err, result) {
+
+        bcrypt.compare(password, result[0].password, function(err, check) {
+          if (check) {
+            res.send("Vous etes connecté")
+          }else{
+            res.render('login',{error : "Mot de passe ou adresse mail incorrecte"})
+          }
+        });
+
+    });
+
+  });
+
 });
 
 module.exports = router;
